18a5e9acf9bc1a4b29883ae75b7ac812e42bd096
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-11-27 18:24:57 +0000

    commit config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+// Variables
+
+const accountId=144551;
+let accountEmail="prashantawati8661@gmail.com"
+ var accounPassword="123456"
+ accountCity="Kolhapur"
+
+
+ //accountId=3       // not allowed
+ accountEmail="prashantawati3033@gmail.com"
+ accountPassword="465456"
+ accountCity="bengaluru"
+ let accountState;
+
+ 
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountState])
+
+/*
+  prefer not to use var because of 
+  issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)       //we are using node.js ,not browser
+
+let name="prashant"
+let age=23
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean=>true/false
+//null => standalone value
+//undefined =>
+    //symbol => unique
+
+
+//object
+
+console.log(typeof age);  // number
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
