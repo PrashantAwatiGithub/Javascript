c6034cbb140b05ec0fcd752f21e249b0eda9b5e9
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-11-28 18:12:59 +0000

    comparision is working

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2 < 3 );  //true
+// console.log(2 <= 3);//true
+// console.log(2>3);  //false
+// console.log(2>=3); //false
+// console.log(3 == 3 );//true
+// console.log(2 != 1);//true
+
+// console.log("2">1);  //ture      /// note -- javascript internally convert the string into the interger
+// console.log("03">5);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+//the reason is that an equality check == and comparisions > < >=  =< works differently.
+//comparison convert null to number , treating it as 0. 
+//that's why (3) null>=0 is true and (1)null > 0 is false.
