732951a651a303f9477bd474893114283560b240
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-11-28 18:32:11 +0000

    done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+//  1 )   primitives
+
+//   7 types : String . number, boolean , null, undefined , symbol , BigInt
+ 
+
+
+// 2)  non- primitive // reference type
+
+//Arrays , Objects , functions
+1.44 
