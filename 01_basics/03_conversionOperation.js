769906a661debb3e9516f35fa5b9217636179391
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-11-28 17:55:18 +0000

    conversion topic done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,7 +17,7 @@
 let isLoggedIn=1;
 let booleanIsLoggedIn=Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0=>false
 //"" => false
@@ -25,7 +25,45 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));  //33 converted in string
+
+//**********************Operations************************ */ 
  
- 
+let value=3
+let negValue= -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+
+let str1="prashant";
+let str2= " Awati";
+
+let str3=str1+str2;
+//console.log(str3);
+
+// console.log("2"+3);
+// console.log(2+"3");
+// console.log("2"+3+4);
+// console.log(2+4+"3");
+// console.log((3+4)* 5 % 4 );
+// console.log(true); //true
+// console.log(+true); // 1
+// console.log(+""); //0
+
+
+num1 = num2 = num3= 2+2;
+
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
