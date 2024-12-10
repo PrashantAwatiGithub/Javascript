c033249377e925b7f931b9f853f25fc072a752d3
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-12-10 17:50:29 +0000

    all datatypes covered

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,10 +1,56 @@
 //  1 )   primitives
 
 //   7 types : String . number, boolean , null, undefined , symbol , BigInt
- 
+
+// const score = 100;
+// console.log(typeof score); //number
+
+// const scorevalue = 1100.3;
+// console.log(typeof scorevalue);   //number
 
 
-// 2)  non- primitive // reference type
+// const isLoggedIn = false;
+// console.log(typeof(isLoggedIn)) // boolean
 
-//Arrays , Objects , functions
-1.44 
+// const outSideTemp = null;
+// console.log(typeof(outSideTemp));  // object
+
+// let userEmail;    // undefined 
+// console.log(typeof(userEmail)); //undefined
+
+// const id = Symbol('1223');
+// console.log(typeof(id));
+
+// const anotherId = Symbol('1223');
+// console.log(typeOf(anotherId));
+
+// console.log(id === anotherId);
+
+// const bigNumber=66546546542156465n;
+// console.log(typeof(bigNumber));
+
+
+//*******************************************
+
+// // 2)  non- primitive // reference type
+
+// //Arrays , Objects , functions
+
+
+
+const heros = ["shaktiman", "junior ji", "Hatim", "VikralGabral"]
+console.log(heros);
+console.log(typeof heros);      // object
+
+const myObject = {
+    name :"prashant",
+    age:24,
+}
+console.log(myObject)
+
+
+// const myfunction=function (){
+// console.log("Hello World");
+// }
+
+//  console.log(typeof myfunction); //objectFunction or function 
