20c0b9d53d119a9ad5914c0e43f2c7422741a14b
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-12-12 05:29:24 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400;
+const balance = new Number(100); //explicitly defining number
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// const otherNumber = 23.856
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++  Maths  +++++++++++++++++++
+//console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,5,69,45,7));
+// console.log(Math.max(45,8,56,84,55,75));
+// console.log(Math.random());  // it gives the number between 0 and 1
+
+// console.log(Math.random()*10); 
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min = 10;
+const max= 20 ;
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min)
+
+
