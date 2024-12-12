28916228e57d4e473f8a346f8a2c88c8d1b41114
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-12-12 03:39:50 +0000

    strings1

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,4 +5,35 @@ const repoCount = 50;
 
 console.log(`my name is ${name} and my repo count is ${repoCount}`);  // this is new methid 
 
+const gameName = new String ('Prashant');
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);  ///8
+// console.log(gameName.toUpperCase());  //PRASHANT
+// console.log(gameName.charAt(4)); // h
+// console.log(gameName.indexOf('t'));  // 7
+const newString= gameName.substring(2,6);
+console.log(newString); //asha
+
+const anotherString= gameName.slice(-8,6);  
+//note : we can use negative value in slice 
+console.log(anotherString); // prasha
+
+const newString1 = "   Prashant    ";
+console.log(newString1);
+console.log(newString1.trim());  // using trim() method we can remove the white spaces
+
+const url="https://prashant.com/prashant%20awati";
+console.log(url.replace('%20','-'));  //https://prashant.com/prashant-awati
+
+console.log(url.includes('prashant'));   ///ture
+
+console.log(url.split('/'));
+
+
+
+
+
+
 
