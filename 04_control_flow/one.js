d87a9dad18dfbf2c202033f873b36e207860b918
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2024-12-31 12:16:19 +0000

    control statements

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,13 +1,29 @@
-const isUserLoggedIn = true
-const tempreture = 41
+//  
+// if(tempreture < 50){
+//     console.log("tempreture is less than 50");
+// }
 
-if(tempreture < 50){
-    console.log("tempreture is less than 50");
-}
-
-console.log("tempreture is greater than 50");
+// console.log("tempreture is greater than 50");
 
 // <,>,<=,>==,==,!=, ===,!==
 
 
- 
+ //-----------------------------------------------
+
+//  const score =200
+//  if(score>100){
+//     let power = "fly";
+//     console.log(`User Power${power}`)
+
+//  }
+//  console.log(`user power ${power}`) // it gives error because the power variable is block scoped that why it is not accessible outside the block
+
+
+//---------------------------------------------------
+
+// const balance = 1000;
+// if(balance>500) console.log("test");    //implicit scope
+
+//-------------------------------------------------
+
+const isUserLoggedIn =true
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+
+// const month =3;
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+
+//         case 2:
+//         console.log("february");
+//         break;
+
+//         case 3:
+//         console.log("march");
+//         break;
+
+//         case 4:
+//         console.log("April");
+//         break;
+
+//     default:console.log("default case mathc")
+//         break;
+// }
+
+
+const month ="april";
+
+switch (  month) {
+    case "january":
+        console.log("january");
+        break;
+
+        case "february":
+        console.log("february");
+        break;
+
+        case "march":
+        console.log("march");
+        break;
+
+        case "april":
+        console.log("April");
+        break;
+
+    default:console.log("default case mathc")
+        break;
+}
