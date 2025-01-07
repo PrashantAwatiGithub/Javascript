bce3b6f805aeeea2e128c26a216a8fce03e23871
PrashantAwatiGithub <131230916+PrashantAwatiGithub@users.noreply.github.com>
2025-01-07 07:22:26 +0000

    loop done

---
+++ 05_iterations/four.js
@@ -0,0 +1,12 @@
+const myObject = {  
+js : 'javascript',
+cpp : 'C++',
+rb : 'rubby',
+swift : 'swift for apple'
+}
+
+for (const key in myObject) {
+    //console.log(key ,myObject[key]);
+
+//console.log(`${key}shortcut is for ${myObject[key]}`)
+}
---
+++ 05_iterations/three.js
@@ -0,0 +1,50 @@
+// for of loop
+ // ["","",""]
+ //[{},{},{}]
+
+// arr = [1,2,3,4,5,6,7,8,9]
+//  for (const num of arr) {
+//     console.log(num)
+    
+//  }
+
+ const greeting = "hello world"
+
+ for (const greet of greeting) {
+    // console.log(greet)
+    //console.log(`each char is  ${greet}`)
+    
+ }
+
+
+
+ //Map
+
+ const map = new Map()
+ map.set('In', "India")
+ map.set('USA',"United states Of America")
+ map.set('FR' , "France")
+ map.set('IN',"India") //map doesn't take duplicate values 
+
+ //console.log(map)
+
+//  for (const element of map) {
+//     console.log(element)
+//  }
+
+for (const [key , value] of map) {//destructuring array
+    //console.log(key , " :-",value);
+}
+
+
+//objects 
+
+const myObject = {
+    'game1': 'NFS',
+    'game2':'spiderMan'
+}
+
+// for (const [key , value] of myObject) {  // this itration method is not work on objects
+//     console.log(key,':-',value);
+// }
+
